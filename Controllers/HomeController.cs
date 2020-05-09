f4edc45e775f1f53e6722baa1879a3c55b4f4d8e
Tonim <lucasrocha1313@gmail.com>
2020-05-09 16:27:29 -0300

    primeiras alteracoes na tela

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,14 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web Mvc App from c# Course";
+            ViewData["Owner"] = "Tonim";
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
