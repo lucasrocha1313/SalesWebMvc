a3bc18e972f4ef1c268047e71ff875dfdf42a2e9
Tonim <lucasrocha1313@gmail.com>
2020-05-17 14:29:04 -0300

    sellers controller

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
